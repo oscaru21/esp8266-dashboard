94714984f983b4d67d4d91fb415761c8cd02e1d5
oscar <oscarumana1021@gmail.com>
2020-08-22 00:18:54 -0600

    add partials and login page

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', function(req, res){ 
+    res.render('home');
+});
+
+app.get('/login', function(req, res){ 
+    res.render('login');
+});
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
