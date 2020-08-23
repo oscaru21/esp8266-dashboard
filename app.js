a0a1666a583fbbc4f3cfad68f8bb96b60e1ced8a
oscar <oscarumana1021@gmail.com>
2020-08-22 20:34:18 -0600

    add authentication, css styles and dashboard with local mqtt broker using Paho

--- app.js
+++ app.js
@@ -15,6 +15,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true, });
+mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get('/', function(req, res){ 
     res.render('home');
 });
@@ -23,7 +51,54 @@ app.get('/login', function(req, res){
     res.render('login');
 });
 
+app.get('/logout', function(req, res){
+    req.logOut();
+    res.redirect('/');
+});
+
+app.get('/about', function(req, res){ 
+    res.render('about');
+});
+
+app.get('/dashboard', function(req, res){ 
+    if(req.isAuthenticated()){
+        res.render('dashboard');
+    }else{
+        res.redirect('/login');
+    }
+});
+
+app.get('/register', function(req, res){
+    const username = "oscarumana1021@gmail.com";
+    const password = "12345";
+    User.register({username: username}, password, function(err, user){
+    if(err){
+        console.log(err);
+    }else{
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/dashboard');
+        })
+    }
+});
+});
+
+app.post('/login', function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/dashboard');
+            });
+        }
+    });
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
+
